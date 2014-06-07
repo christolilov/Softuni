4b1ea48075d5bdd510462f5b522068f73799802a
christolilov <hristolilov1@gmail.com>
2014-06-07 17:48:59 +0300

    A few commits

---
+++ C#BasicsHomeworks/03OpeatorsExpressionsStatements/01OddOrEven/OddOrEven.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Write an expression that checks if given integer is odd or even.
+class OddOrEven
+{
+    static void Main()
+    {
+        Console.WriteLine("This program will calculate if a given number is odd or even!");
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int remainder = number % 2;
+        string result = (remainder == 0 ? "even" : "odd");
+        Console.WriteLine("The number you have entered is " +result+"!");
+    }
+}
