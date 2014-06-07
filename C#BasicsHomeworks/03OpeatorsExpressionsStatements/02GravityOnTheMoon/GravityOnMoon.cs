3230bad1d5db6408fd6bf808c4b935c46068d990
christolilov <hristolilov1@gmail.com>
2014-06-07 17:50:50 +0300

    C-Sharp-Basics-Homeworks

---
+++ C#BasicsHomeworks/03OpeatorsExpressionsStatements/02GravityOnTheMoon/GravityOnMoon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//The gravitational field of the Moon is approximately 17% of that on the Earth. Write a program that calculates the weight of a man on the moon 
+//by a given weight on the Earth.
+class GravityOnMoon
+{
+    static void Main()
+    {
+        Console.WriteLine("This program will calculate your weight on the moon!");
+        Console.Write("Please enter your weight on Earth: ");
+        double weight = double.Parse(Console.ReadLine());
+        double weightOnMoon = 0.17 * weight;
+        Console.WriteLine("Your weight on the moon is " +weightOnMoon+ "!");
+    }
+}
---
+++ C#BasicsHomeworks/03OpeatorsExpressionsStatements/03DivideBySevenAndFive/DivideBy7and5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Write a Boolean expression that checks for given integer if it can be divided (without remainder) by 7 and 5 in the same time. 
+class DivideBySevenAndFive
+{
+    static void Main()
+    {
+        Console.WriteLine("This program will check if a given number can be divided by 7 and 5 in the same time without remainder!");
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        bool remainder1 = number %5 ==0;
+        bool remainder2 = number %7 ==0;
+        Console.WriteLine(remainder1 && remainder2 ? "The given number can be divided without remainder by 7 and 5!" : "The given number cannot be divided without remainder by 7 and 5!");
+    }
+}
